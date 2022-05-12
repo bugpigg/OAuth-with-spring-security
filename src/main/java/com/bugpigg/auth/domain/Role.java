ebc6eba18f1721d7e159c5c4356b703f5279c929
bugpigg <bugpigg@gmail.com>
2022-05-12 13:51:39 +0900

    feat: Role 도메인 추가
    - User의 Role을 나타내는 Enum Class 인 Role 클래스 추가

---
+++ src/main/java/com/bugpigg/auth/domain/Role.java
@@ -0,0 +1,17 @@
+package com.bugpigg.auth.domain;
+
+public enum Role {
+    GUEST("ROLE_GUEST"),
+    USER("ROLE_USER");
+
+    private final String key;
+
+    Role(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+}
